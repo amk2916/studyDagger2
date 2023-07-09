44f4286ffea8bcf64af88a3edf8ef74d788d7a3b
Alexey Kozyrev <tcsid4@gmail.com>
2023-07-09 14:29:50 +0300

    Реализовали модули через binds

--- app/src/main/java/com/example/startdagger2/example2/di/DataModule.kt
+++ app/src/main/java/com/example/startdagger2/example2/di/DataModule.kt
@@ -4,16 +4,18 @@ import com.example.startdagger2.example2.data.datasource.ExampleLocalDataSource
 import com.example.startdagger2.example2.data.datasource.ExampleLocalDataSourceImpl
 import com.example.startdagger2.example2.data.datasource.ExampleRemoteDataSource
 import com.example.startdagger2.example2.data.datasource.ExampleRemoteDataSourceImpl
-import com.example.startdagger2.example2.data.repository.ExampleRepositoryImpl
-import com.example.startdagger2.example2.domain.ExampleRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
-class DataModule {
-    @Provides
-    fun provideExampleLocalDataSource(impl: ExampleLocalDataSourceImpl): ExampleLocalDataSource = impl
+interface DataModule {
+    @Binds
+     fun bindExampleLocalDataSource(
+        impl: ExampleLocalDataSourceImpl)
+    : ExampleLocalDataSource
 
-    @Provides
-    fun provideExampleRemoteDataSource(impl:ExampleRemoteDataSourceImpl): ExampleRemoteDataSource = impl
+    @Binds
+     fun bindExampleRemoteDataSource(
+        impl:ExampleRemoteDataSourceImpl
+    ): ExampleRemoteDataSource
 }
--- app/src/main/java/com/example/startdagger2/example2/di/DomainModule.kt
+++ app/src/main/java/com/example/startdagger2/example2/di/DomainModule.kt
@@ -2,12 +2,14 @@ package com.example.startdagger2.example2.di
 
 import com.example.startdagger2.example2.data.repository.ExampleRepositoryImpl
 import com.example.startdagger2.example2.domain.ExampleRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
-class DomainModule {
-    @Provides
-    fun provideExampleRepository(impl: ExampleRepositoryImpl): ExampleRepository = impl
+interface DomainModule {
+    @Binds
+    fun bindExampleRepository(
+        impl: ExampleRepositoryImpl
+    ): ExampleRepository
 
 }
